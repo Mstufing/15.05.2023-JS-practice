db09d13ec215b71c39f5fd9fbdac4390e99c67f3
Mstufing <mstufingms11@gmail.com>
2023-05-15 18:33:45 +0300

    easy tasks #1, 2, 3, 4, 5, 6 && 7

--- js/main.js
+++ js/main.js
@@ -2,23 +2,6 @@
 // todo ----- VERY EASY -----
 
 // ? Task #1
-// ? How Many Vowels?
-
-// function countVowels(str) {
-//     let arr = str.match(/[aeiou]/gi);
-//     return arr.length;
-// }
-
-// let btn = document.querySelector('.btn');
-
-// btn.onclick = () => {
-//     let inp = document.querySelector('.inp');
-//     let result = document.querySelector('.result');
-
-//     result.innerHTML = countVowels(inp.value);
-// }
-
-// ? Task #2
 // ? Recursion to Repeat a String n Number of Times
 // ! Task should solved in Recursion !!!
 // todo Try to solve it in another way!
@@ -37,7 +20,114 @@
 //     result.innerHTML = repetition(firstInp.value, secondInp.value);
 // }
 
+
+// ? Task #2
+// ? Say Hello to Guests
+
+// function greetPeople(names) {
+//     let arr = names.map(elem => `Hello ${elem}`);
+//     return arr.join(', ')
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let firstInp = document.querySelector('.inp1');
+//     let result = document.querySelector('.result');
+
+//     result.innerHTML = greetPeople(firstInp.value.trim().split(' '));
+// }
+
+
+// todo ----- EASY -----
+
+// ? Task #1
+// ? Hashes and Pluses
+
+// function hashPlusCount(str) {
+//     let hashArr = str.split('').filter(elem => elem === '#');
+//     let plusArr = str.split('').filter(elem => elem === '+');
+//     return [hashArr.length, plusArr.length];
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let firstInp = document.querySelector('.inp1');
+//     let result = document.querySelector('.result');
+
+//     result.innerHTML = hashPlusCount(firstInp.value);
+// }
+
+// ? Task #2
+// ? Secret Society
+
+// function societyName(friends) {
+//     let myArr = friends.map(elem => elem[0]);
+//     return myArr.sort().join('')
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let myInp = document.querySelector('.inp');
+//     let result = document.querySelector('.result');
+
+//     result.innerHTML = societyName(myInp.value.trim().split(' '));
+// }
+
 // ? Task #3
+// ? Reverse the Case
+
+// function reverseCase(str) {
+//     let arr = str.split('').map(elem => elem == elem.toLowerCase() ? elem.toUpperCase() : elem.toLowerCase())
+//     return arr.join('');
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let firstInp = document.querySelector('.inp1');
+//     let result = document.querySelector('.result');
+
+//     result.innerHTML = reverseCase(firstInp.value);
+// }
+
+// ? Task #4
+// ? Repeating Letters
+
+// function doubleChar(str) {
+//     let myArr = str.split('').map(elem => elem.repeat(2))
+//     return myArr.join('');
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let firstInp = document.querySelector('.inp1');
+//     let result = document.querySelector('.result');
+
+//     result.innerHTML = doubleChar(firstInp.value);
+// }
+
+// ? Task #5
+// ? How Many Vowels?
+
+// function countVowels(str) {
+//     let arr = str.match(/[aeiou]/gi);
+//     return arr.length;
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let inp = document.querySelector('.inp');
+//     let result = document.querySelector('.result');
+
+//     result.innerHTML = countVowels(inp.value);
+// }
+
+// ? Task #6
 // ? Find the Bomb
 
 // function bomb(str) {
@@ -57,7 +147,7 @@
 //     result.innerHTML = bomb(firstInp.value);
 // }
 
-// ? Task #4
+// ? Task #7
 // ? Return the Index of the First Vowel
 
 // // function firstVowel(str) {
